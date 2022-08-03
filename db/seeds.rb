22e84739129f9def9342028b141d99582144b5bb
keoshayoung <keoshayoung@email.com>
2022-08-03 04:28:25 -0400

    creates Genre and MovieGenre models

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,9 @@ actor.save
 
 movie = Movie.new({ title: "Where the Crawdads Sing", year: 2022, plot: "Where the Crawdads Sing is an upcoming American mystery drama film, based on the novel of the same name by Delia Owens, directed by Olivia Newman from a screenplay written by Lucy Alibar, and produced by Reese Witherspoon. The film stars Daisy Edgar-Jones, Taylor John Smith, Harris Dickinson, Michael Hyatt, Sterling Macer, Jr., and David Strathairn.", director: "Olivia Newman" })
 movie.save
+
+genre = Genre.new({ name: "Drama" })
+genre.save
+
+genre = Genre.new({ name: "Action" })
+genre.save
