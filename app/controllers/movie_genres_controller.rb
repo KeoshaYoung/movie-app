a269537b45e0af447982ff6d405df76fea7eaa0b
keoshayoung <keoshayoung@email.com>
2022-08-03 04:55:27 -0400

    adds create action for movie_genre

--- app/controllers/movie_genres_controller.rb
+++ app/controllers/movie_genres_controller.rb
@@ -1,2 +1,10 @@
 class MovieGenresController < ApplicationController
+  def create
+    movie_genre = Movie_genre.new(
+      movie_id: params["movie_id"],
+      genre_id: params["genre_id"],
+    )
+    movie_genre.save
+    render json: movie_genre.as_json
+  end
 end
